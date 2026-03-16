036c7eb5dabc4202829415ea57801c8292ab94c9
infy_mac <infy_mac@infy-macui-Macmini.local>
2026-03-16 21:42:47 +0530

    BrainLogic 수정

--- Framework/Script/Entity/Brain/BrainLogic.cs
+++ Framework/Script/Entity/Brain/BrainLogic.cs
@@ -2,10 +2,10 @@ using UnityEngine;
 
 public static class BrainLogic
 {
-    public static Brain CreateBrainAndEntity(Entity ownerEntity, string brainPath, string entityPath)
+    public static Brain CreateBrainAndEntity(Entity ownerEntity, string brainPath, string entityPath, IInitData brainData = null, IInitData entityData = null)
     {
-        var brain = ownerEntity.AddEntity<Brain>(brainPath);
-        var entity = ownerEntity.AddEntity<Entity>(entityPath);
+        var brain = ownerEntity.AddEntity<Brain>(brainPath, brainData);
+        var entity = ownerEntity.AddEntity<Entity>(entityPath, entityData);
         brain.AttachControll(entity);
 
         return brain;
